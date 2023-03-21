b3e19fd783bcc01b7cd1cb51af6ea0b41c1012a2
Bruno Oliveira <bds.commus@gmail.com>
2023-03-20 22:04:03 -0300

    fix: remove constrained

--- database/migrations/2023_03_21_004024_add_column_to_users_table.php
+++ database/migrations/2023_03_21_004024_add_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignUuid('tenant_id')->constrained()->after('name');
+            $table->foreignUuid('tenant_id')->after('name');
         });
     }
 
